da0df608f64e29fed23bc4e57808579f362e828d
Wolfgang Lugmayr <w.lugmayr@luxnet.at>
2022-03-01 10:49:52 +0100

    Initialization

---
+++ examples/00_templates/starmap_gnu_parallel_local.tmpl.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# This StarMap template uses GNU parallel to compute multiple models on available CPUs
+
+echo "### start: " $(date)
+echo "### node : " $(hostname)
+
+parallel -j @@CORES@@ \
+'@@ROSETTA_SCRIPT_EXE@@ \
+-parser:protocol "@@ROSETTA_SCRIPT_FILE@@" \
+-edensity:mapfile "@@DENSITY_FILE@@" \
+-s "@@INPUT_PDB_FILE@@" \
+-in::file::centroid_input \
+-mapreso @@HIRES@@ \
+-nstruct 1 \
+-default_max_cycles 200 \
+-crystal_refine \
+-edensity::cryoem_scatterers true \
+-ignore_unrecognized_res \
+-exclude_dna_dna false \
+-out:level 200 \
+-out:no_nstruct_label \
+-out:pdb \
+-out:suffix _`printf %04d {}`
+' ::: {1..@@NSTRUCT@@}
+
+echo "### end: " $(date)
+
